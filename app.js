fea1a43bfd7ed9df3a63d3eaa15103b8ad423f3d
nicokurogane <fyedream@gmail.com>
2019-07-11 08:25:02 -0600

    Added confirm delete funcitonality

--- app.js
+++ app.js
@@ -1,57 +1,52 @@
 import Request from "./io/request.js";
-import HtmlRender from "./ui/htmlrender.js";
+import IndexUIRender from "./ui/indexuirender.js";
 import PostList from "./ui/postlist.js";
 
 let requestHandler = new Request();
-let htmlrenderHandler = new HtmlRender();
+let uiRenderHandler = new IndexUIRender();
 let postList = new PostList();
 
 function deletePost(id) {
-  requestHandler
-    .deletePostById(id)
-    .then(result => {
+  requestHandler.deletePostById(id).then(result => {
       window.location.reload();
     })
     .catch(err => console.log(err));
 }
 
-requestHandler
-  .getLast3Posts()
+requestHandler.getLast3Posts()
   .then(array => {
     let lowestId = 0;
     array.forEach(element => {
       if (lowestId === 0 || element.id < lowestId) lowestId = element.id;
     });
     lowestId--; //quitamos uno por la manera de contar de la API para los limites del request
-    htmlrenderHandler.renderFeaturePosts(array, deletePost);
+    uiRenderHandler.renderFeaturePosts(array, deletePost);
     return lowestId;
   })
   .then(id => {
     requestHandler.getPosts(0, id).then(array => {
       array = array.reverse();
       postList.posts = array;
-      htmlrenderHandler.renderOtherPost(array, deletePost);
+      uiRenderHandler.renderOtherPost(array, deletePost);
     });
   });
 
 //tag loading
 requestHandler.getTags().then(tags => {
-  htmlrenderHandler.renderTagsList(tags);
+  uiRenderHandler.renderTagsList(tags);
 });
 
 document.getElementById("btn-filter-by-tag").addEventListener("click", e => {
-  let filteredPost = postList.filterPostsByTags(
-    htmlrenderHandler.getFiltersTags()
-  );
-  htmlrenderHandler.resetPostList();
-  htmlrenderHandler.renderOtherPost(filteredPost);
+  let filteredPost = postList.filterPostsByTags( uiRenderHandler.getFiltersTags() );
+  uiRenderHandler.resetPostList();
+  uiRenderHandler.renderOtherPost(filteredPost);
 });
 
 document.getElementById("post-title-search-form").addEventListener("submit", e => {
     e.preventDefault();
     let filter = document.getElementById("title-filter").value;
     requestHandler.getFilteredPost(filter).then(arrayPost => {
-      htmlrenderHandler.resetPostList();
-      htmlrenderHandler.renderOtherPost(arrayPost);
+      uiRenderHandler.resetPostList();
+      uiRenderHandler.renderOtherPost(arrayPost);
     });
 });
--- ui/indexuirender.js
+++ ui/indexuirender.js
@@ -1,4 +1,4 @@
-export default class HtmlRender {
+export default class IndexUIRender {
   constructor() {
     this.featurePostContainer = document.getElementById("feature-post-container");
     this.postContainer = document.getElementById("post-container");
@@ -15,12 +15,10 @@ export default class HtmlRender {
             <h5 class="card-title">${post.title}</h5>
             <p class="card-text">${post.subTitle}</p>
             <a class="btn btn-primary" href="./view_post.html?id=${post.id}">Check Post</a>
-            <a class="btn btn-success" href="./post.html?id=${post.id}">Edit Post</a>            
+            <a class="btn btn-success" href="./post.html?id=${post.id}">Edit Post</a>          
           </div>
         </div>`;
 
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("col-sm-4");
       newCard = new DOMParser().parseFromString(newCard, "text/html");
 
       const deleteIcon = document.createElement("a"); 
@@ -32,6 +30,9 @@ export default class HtmlRender {
       });
 
       newCard.querySelector(".card-body").appendChild(deleteIcon);
+
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("col-sm-4");
       newDiv.appendChild(newCard.documentElement);
       this.featurePostContainer.appendChild(newDiv);
     });
@@ -39,14 +40,14 @@ export default class HtmlRender {
 
   renderOtherPost(posts, deleteFunction) {
     posts.forEach(post => {
+      let divConfirmationId = `post-${post.id}`;
       let newCard = `
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">${post.title}</h5>
             <p class="card-text">${post.subTitle}</p>
             <a class="btn btn-primary" href="./view_post.html?id=${post.id}">Check Post</a>
-            <a class="btn btn-success" href="./post.html?id=${post.id}">Edit Post</a>
-            
+            <a class="btn btn-success" href="./post.html?id=${post.id}">Edit Post</a>            
           </div>
         </div>`;
       const newDiv = document.createElement("div");
@@ -59,10 +60,13 @@ export default class HtmlRender {
       deleteIcon.classList.add("btn", "btn-danger");
       deleteIcon.innerText = "delete post";
       deleteIcon.addEventListener("click", e=>{
-        deleteFunction(e.target.dataset.id);
+        let postId = e.target.dataset.id; 
+        document.getElementById(divConfirmationId).classList.add("show");
       });
       
       newCard.querySelector(".card-body").appendChild(deleteIcon);
+
+      this.createDeleteConfirmation( newCard.querySelector(".card-body"), divConfirmationId,deleteFunction,post.id );
       newDiv.appendChild(newCard.documentElement);
       this.postContainer.appendChild(newDiv);
     });
@@ -103,4 +107,39 @@ export default class HtmlRender {
   getFiltersTags() {
     return this.filterTagList.map(tag => Number(tag));
   }
+
+  /*creates a div inside the div containing the post info*/
+  createDeleteConfirmation(postDiv, divId, deleteFunction, postId){
+    const newDiv = document.createElement("div");
+    newDiv.id = divId;
+    newDiv.classList.add("col-sm-12","confirmation-message");
+
+    let message = `<div>Are you sure you want to delete this post?</div>`;
+    message = new DOMParser().parseFromString(message, "text/html");
+
+    let deleteButton = document.createElement("button");
+    deleteButton.dataset.id = postId;
+    deleteButton.classList.add("btn","btn-danger");
+    deleteButton.innerText = "delete post";
+    deleteButton.addEventListener("click",e=>{
+        deleteFunction(e.target.dataset.id);
+        window.location.reload();
+    });
+
+    let cancelButton= document.createElement("button");
+    cancelButton.classList.add("btn","btn-secondary");
+    cancelButton.innerText = "cancel";
+    cancelButton.addEventListener("click", e=>{
+      document.getElementById(divId).classList.remove("show");
+    });
+
+
+    newDiv.appendChild(message.documentElement);
+    newDiv.appendChild(cancelButton);
+    newDiv.appendChild(deleteButton);
+
+    postDiv.appendChild(newDiv);
+  }
+
+
 }
