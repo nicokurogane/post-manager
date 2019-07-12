7bc7b173f04377ce17e3398c6b519e94292931ad
nicokurogane <fyedream@gmail.com>
2019-07-11 22:14:45 -0600

    Add visual changes to create/edit post

--- postpage.js
+++ postpage.js
@@ -5,18 +5,18 @@ import Request from "./io/request.js";
 const requestHandler = new Request();
 const formHandler = new FormUI();
 
-
-
 //de esta manera podemos buscar si se le ha pasado algun parametro a la pagina
-
 let params = new URLSearchParams(location.search);
 let idToEdit = params.get("id");
 
+
 if (!idToEdit) {
   document.getElementById("btnEdit").style.display = "none";
+  document.getElementById("screen-title").innerText="Create Post";
   formHandler.initFields(requestHandler);
 } else {
   document.getElementById("btnCreate").style.display = "none";
+  document.getElementById("screen-title").innerText="Edit Post";
   //extraemos el POST a modificar
   requestHandler.getPostById(idToEdit).then(post => {
     formHandler.initFieldsWithPost(requestHandler, post);
